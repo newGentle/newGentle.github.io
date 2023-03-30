f4fe81f3800a312a662a800b81d9596a96ba7321
Akbar <abdullaev.akbar@gmail.com>
2023-03-30 16:37:53 +0500

    Add files via upload

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -11,9 +11,15 @@ coll.forEach((item) => {
         allActives.forEach((i) => {
             if (i.classList.item(0) != item.classList.item(0) + '-content') {
                 i.classList.remove('active');
+                coll.forEach(el => {
+                    if (el.classList.item(0) != item.classList.item(0)) {
+                        el.classList.remove('opened')
+                    }});
             }
         });
 
+
+        item.classList.toggle('opened');
         content.classList.toggle('active');
     })
 })
